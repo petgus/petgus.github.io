f974a86ec48e61a6735b1b97c69840f020d2e335
petgus <the.peter.gustafsson@gmail.com>
2024-05-21 20:26:46 +0200

    Markup and fetching facts from API

---
+++ script.js
@@ -0,0 +1,41 @@
+
+var factButton = document.getElementById('factButton');
+
+factButton.addEventListener('click', getCatFactsFromAPI);
+
+
+var factDisplay = document.getElementById('factDisplay');
+
+var loadingSpinner = document.getElementById('loading-spinner');
+
+function getCatFactsFromAPI() {
+  factButton.disabled = true; // Disable the button
+
+  loadingSpinner.style.display = "block"; // Show the loading spinner
+  factDisplay.style.display = "none"; // Hide the fact display
+
+  // wait for 1 second before fetching the data
+  setTimeout(function () {
+
+    fetch('https://catfact.ninja/fact')
+    .then(response => response.json())
+    .then(data => {
+
+        // Hide the loading spinner
+        loadingSpinner.style.display = "none";
+        factDisplay.style.display = "inline-block";
+
+        // Set the innerText of factDisplay to the fact text
+        factDisplay.innerText = data.fact;
+
+        factButton.disabled = false; // Enable the button again
+    });
+
+
+
+  }, 1000);
+
+ 
+}
+
+getCatFactsFromAPI()
